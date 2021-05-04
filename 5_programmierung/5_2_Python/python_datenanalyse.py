e82b02c43812f820f46dfe1458fdd2f07d42a76d
Chantal <chantal.gaertner@uni-greifswald.de>
2021-05-04 12:12:13 +0200

    Ergänzung Python Datenanalyse

--- 5_programmierung/5_2_Python/python_datenanalyse.py
+++ 5_programmierung/5_2_Python/python_datenanalyse.py
@@ -1,18 +1,137 @@
-#Datensatz laden
+# Pandas importieren
 import pandas as pd
-df = pd.read_csv("example-twitter.csv",sep=';')
+
+
+# Datensatz laden
+df = pd.read_csv("example-twitter.csv", sep=';')
+
+# Würde der Datensatz als Excel-Datei vorliegen, 
+# ließe er sich mit folgendem Befehl einlesen:
+# df = pd.read_csv("example-twitter.xlsx")
+
 
 # Datensatz anzeigen
 display(df)
 
-# Datensatz speichern
+
+# Datensatz speichern als CSV-Datei
 df.to_csv("example-twitter-neu.csv", index=False)
 
+
+# Leeren Datensatz erstellen und mit Werten füllen 
+# hier: Liste aus Dictionaries, die in "data" abgelegt wird
+df_new = pd.DataFrame()
+
+data = [{'id':1,'name':'rey'}, {'id':2,'name':'han'}]
+
+df_new = df_new.append(data, sort=False, ignore_index=True)
+
+
+# Objekt löschen 
+del df_new
+
+
+# Funktionen zum Auswählen von Zeilen, Spalten und Fällen 
+
+# Auswählen der Spalte "replies" aus "df"
+df.loc[:,'replies']
+
+
+# Auswählen mehrerer Spalten (einer Liste von Spalten)
+df.loc[:,['replies','favorites']]
+
+
+# Auswahl von Spalten nach Bereich
+df.loc[:,'replies':'retweets']
+
+
 # Zeilenindex setzen (statt Zeilennummern)
-df = df.set_index("from")
+# Zurücksetzen von Zeilenindizes über df = df.reset_index() möglich.
+df = df.set_index(['from'])
 
-# Mittels Namen von Zeilen und Spalten auswählen
-df.loc[['dagobah'], ['replies']]
 
-# Mittels Positionen auswählen
+# Auswählen aller Zeilen mit dem Wert "dagobah" aus der indexierten Spalte "from"
+df.loc['dagobah']
+
+
+# Einschränken des Datensatzes auf Zeilen und Spalten
+df_subset = df.loc[['dagobah','exogol'], ['favorites']]
+
+
+# Zeilen und Spalten mittels Positionen auswählen
 df.iloc[:5,2:]
+
+
+# Filtern von Werten
+df[df.favorites > 10]
+
+
+# Kombinieren von Bedingungen beim Filtern von Werten 
+df[(df.favorites > 10) & (df.retweets > 5)]
+
+
+# Aneinanderketten von Funktionen 
+auswahl = df[df.favorites > 10].loc[:,['replies','favorites']]
+
+
+# Filter der Spalte "hashtag" durch regulären Ausdruck 
+# (=Formuliereung von Suchmustern, die in der entsprechenden Zeile 
+# vorhanden sein müssen).
+df['hashtags'].str.contains("natur|klima|umwelt", case=False, regex=True)
+
+
+# Erstellen der neuen Spalte "natur"
+# Die Spalte enthält true- und false-Werte, je nachdem, 
+# ob der Hashtag "natur" in einer Zeile auftaucht oder nicht.
+df['natur'] = df['hashtags'].str.contains("natur|klima|umwelt", case = False, regex = True)
+
+
+# Erstellen der neuen Spalte "engagement"
+# durch addieren der "retweets" und "replies"
+df['engagement '] = df['retweets'] + df['replies']
+
+
+# Ersetzen von fehlenden Werten (nan) mit Nullen
+df['retweets'] = df['retweets'].fillna(0)
+      
+
+# Funktionen zum Aggregieren und Auszählen
+
+# Auszählen, wie oft die Autor:innen in der Spalte 
+# "from" vorkommen. 
+# Achtung: Diese Spalte darf nicht indexiert sein. 
+# Gegebenenfalls muss die Indexierung entfernt werden.
+df['from'].value_counts()   
+
+
+# Erstellen einer Kreuztabelle
+pd.crosstab(df['from'],df['natur'])      
+
+
+# Ermitteln von Mittelwerten von einer oder mehreren Spalten
+df['favorites'].mean()
+df[['retweets','replies']].mean()
+
+# Ermitteln von Mittelwerten je Zeile 
+df[['retweets','replies']].mean(axis=1)
+
+
+# Übersicht über Werte (u.a. Standardabweichung, Minimum, Maximum)
+#aller zahlenbasierter Spalten
+df.describe()
+
+
+# Vergleiche von Gruppen: 
+# - Größe
+# - Mittelwerte
+# - allgemeine Übersicht 
+df.groupby('from').size()
+df.groupby('from')[['retweets','replies']].mean() 
+df.groupby('from')['favorites'].describe()
+
+
+# Abspeichern von Auswertungen in neuem 
+# Datensatz; Überführen des Indexes in eine eigene Spalte 
+df_fav = df.groupby('from')['favorites'].mean()
+df_fav = df_fav.reset_index()
+df_fav.to_csv("favorites_mean.csv", index = False)
