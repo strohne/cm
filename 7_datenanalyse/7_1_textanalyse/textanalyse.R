b00cd41f820965d5e218c4425349348d5e82b02f
strohne <krimskram@freenet.de>
2021-05-21 02:16:10 +0200

    Add log likelihood ratio

--- 7_datenanalyse/7_1_textanalyse/textanalyse.R
+++ 7_datenanalyse/7_1_textanalyse/textanalyse.R
@@ -110,27 +110,31 @@ tfidf <- woerter %>%
 
 
 #
-# Positive Pointwise Mutial Information berechnen (PPMI) ----
+# Pointwise Mutial Information berechnen (PMI) ----
 #
 
 pmi <- woerter %>% 
   pairwise_count(wort,doc_id) %>% 
   
+  # Auftretenswahrscheinlichkeit der Kombination
   mutate(p = n / sum(n)) %>%
   
+  # Auftretenswahrscheinlichkeit für Wort 1
   group_by(item1) %>% 
   mutate(n1=sum(n)) %>% 
   ungroup() %>% 
   mutate(p1=n1/sum(n)) %>% 
   
+  # Auftretenswahrscheinlichkeit für Wort 2
   group_by(item2) %>% 
   mutate(n2=sum(n)) %>% 
   ungroup() %>% 
   mutate(p2=n2/sum(n)) %>% 
   
-  mutate(pmi = log(p / (p1 * p2)),
-         ppmi = pmax(pmi,0))
+  # Berechnung von pmi 
+  mutate(pmi = log(p / (p1 * p2)))
 
+    
 
 #
 # N-Gramme und Skip-Gramme analysieren ----
@@ -141,13 +145,13 @@ pmi <- woerter %>%
 bigrams <- texte %>% 
   unnest_tokens(bigram,text,token="ngrams",n=2)
 
-# Kollokationen zählen (pairwise_count)
+# Kookkurrenz zählen (pairwise_count)
 bigrams  <- bigrams %>%
   mutate(bigram_no = row_number()) %>% 
   unnest_tokens(wort, bigram) %>% 
   pairwise_count(wort, bigram_no)
 
-# Positive Pointwise Mutial Information berechnen (PPMI)
+# Pointwise Mutial Information berechnen (PMI)
 bigrams <- bigrams %>%
   
   # Auftretenswahrscheinlichkeit des Bigrams
@@ -166,8 +170,12 @@ bigrams <- bigrams %>%
   mutate(p2=n2/sum(n)) %>% 
   
   # Berechnung von pmi
-  mutate(pmi = log(p / (p1 * p2)),
-         ppmi = pmax(pmi,0)) 
+  mutate(pmi = log(p / (p1 * p2))) 
+
+
+# Wie oft kommen Einzelfallkombinationen vor?
+bigrams %>% 
+  count(n) 
 
 
 # Skip-Gramme ----
@@ -177,28 +185,88 @@ bigrams <- bigrams %>%
 skipgrams <- texte %>% 
   unnest_tokens(skipgram,text,token="skip_ngrams",n=2,n_min=2,k=5)
 
-# Kollokationen zählen (pairwise_count)
+# Kookkurrenz zählen (pairwise_count)
 skipgrams  <- skipgrams %>%
   mutate(no = row_number()) %>% 
   unnest_tokens(wort, skipgram) %>% 
   anti_join(stoppwoerter) %>% 
   pairwise_count(wort, no, diag = F, upper=T,sort = TRUE)
 
-# Positive Pointwise Mutial Information berechnen (PPMI)
+# Pointwise Mutial Information berechnen (PMI)
 skipgrams <- skipgrams %>%
+  
+  # Auftretenswahrscheinlichkeit des Skipgrams
   mutate(p = n / sum(n)) %>%
   
+  # Auftretenswahrscheinlichkeit für Wort 1
   group_by(item1) %>% 
   mutate(n1=sum(n)) %>% 
   ungroup() %>% 
   mutate(p1=n1/sum(n)) %>% 
   
+  # Auftretenswahrscheinlichkeit für Wort 2
   group_by(item2) %>% 
   mutate(n2=sum(n)) %>% 
   ungroup() %>% 
   mutate(p2=n2/sum(n)) %>% 
   
-  mutate(pmi = log(p / (p1 * p2)),
-         ppmi = pmax(pmi,0))
+  # Berechnung von pmi
+  mutate(pmi = log(p / (p1 * p2)))
 
+
+
+
+
+
+#
+# Log-Likelihood-Ratio (Dunning, 1993) ----
+#
+
+# Siehe auch http://tdunning.blogspot.com/2008/03/surprise-and-coincidence.html
+
+# Als Vorbereitung zur Berechnung des Log-Likelihood-Ratio wird
+# eine Funktion zur Berechnung der Shannon-Entropy H definiert
+# k = Vektor mit Ereignisanzahlen
+entropy <- function(k) {
+  N = sum(k)
+  return (sum(k/N * log(k/N + (k==0))))
+}
+
+# Wesentliche Grundlage der Berechnung ist für immer zwei Wörter:
+# - wie häufig sie zusamen auftreten (n)
+# - wie häufig sie jeweils ohne das andere Wort auftreten (n1not2, n2not1)
+# - wie häufig andere Wörter gemeinsam auftreten
+
+llr <- woerter %>% 
+
+  # Anzahl der Kombination = n
+  pairwise_count(wort,doc_id) %>% 
+  
+  # Anzahl für Wort 1 = n1
+  group_by(item1) %>% 
+  mutate(n1=sum(n)) %>% 
+  ungroup() %>% 
+  
+  # Anzahl  für Wort 2 = n2
+  group_by(item2) %>% 
+  mutate(n2=sum(n)) %>% 
+  ungroup() %>% 
+  
+  # Anzahl Wort 1 ohne Wort 2 = n1not2
+  mutate(n1not2 = n1 - n) %>%
+  
+  # Anzahl Wort 2 ohne Wort 1 = n2not1
+  mutate(n2not1 = n2 - n) %>%
+  
+  # Kombinationen ohne Wort 1 und ohne Wort 2 = not12
+  mutate(not12 = sum(n) - n1 - n2not1) %>% 
+  
+  # Log-Likelohood-Ratio (G^2)
+  mutate(llr= 2 * n * (
+    entropy(c(n, n2not1, n1not2, not12)) -
+      entropy(c(n2, n1not2) ) - 
+      entropy(c(n1, n2not1))
+    )
+  ) %>% 
+  arrange(-llr)
 
